3d7f94a058369ca1f3fddca97f1e341e7faa215d
Manu_Dandey2 <91964331+ManuFake@users.noreply.github.com>
2022-10-05 02:35:10 +0530

    Add files via upload (#4)

---
+++ realloc.c
@@ -0,0 +1,21 @@
+//use of realloc             *** print value by user ***
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *ptr, n;
+    printf("Enter the size of the array you want to create :  ");
+    scanf("%d", &n);
+    ptr = (int *) realloc(ptr, n * sizeof(int));                  // realloc 
+    for(int i=0; i<n; i++)
+    {
+        printf(" \n Enter the value no %d of this array :  ", i+1);
+        scanf("%d", &ptr[i]);
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        printf("\n The value at %d of this array is %d", i+1, ptr[i]);
+    }
+return 0;
+}
