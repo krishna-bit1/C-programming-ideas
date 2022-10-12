19ad449a0d5d66dbe198b48f17bbef0d984a10ae
Adya Pandey <73730373+AdyaPandey01@users.noreply.github.com>
2022-10-12 14:09:27 +0530

    Create SENTENCE.C

---
+++ SENTENCE.C
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char sentence[1000];
+
+    // creating file pointer to work with files
+    FILE *fptr;
+
+    // opening file in writing mode
+    fptr = fopen("program.txt", "w");
+
+    // exiting program 
+    if (fptr == NULL) {
+        printf("Error!");
+        exit(1);
+    }
+    printf("Enter a sentence:\n");
+    fgets(sentence, sizeof(sentence), stdin);
+    fprintf(fptr, "%s", sentence);
+    fclose(fptr);
+    return 0;
+}
+
+// NEW PROGRAM
+
+#include <stdio.h>
+int main() {
+   int i, j, rows;
+   printf("Enter the no. of rows: ");
+   scanf("%d", &rows);
+   for (i = rows; i >= 1; --i) {
+      for (j = 1; j <= i; ++j) {
+         printf("* ");
+      }
+      printf("\n");
+   }
+   return 0;
+}
